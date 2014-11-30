674f33f13f5f41f43b3a120d99e13ea6f7df606a
Mamad Purbo <m.purbo@gmail.com>
2014-11-30 10:39:24 +0900

    starter project

---
+++ PurboSwiftCsv/Parser.swift
@@ -0,0 +1,82 @@
+//
+//  Parser.swift
+//  PurboSwiftCsv
+//
+//  Created by Purbo Mohamad on 11/29/14.
+//  Copyright (c) 2014 Purbo. All rights reserved.
+//
+
+import Foundation
+
+public class Format {
+    
+    class func defaultFormat() -> Format {
+        return Format()
+    }
+    
+    public init() {
+        
+    }
+    
+}
+
+extension NSInputStream
+{
+    public func readString(length:Int) -> String {
+        
+        var str = ""
+        
+        if length > 0 {
+            var readBuffer = UnsafeMutablePointer<UInt8>.alloc(length+1)
+            
+            var numberOfBytesRead = self.read(readBuffer, maxLength: length)
+            if numberOfBytesRead == length {
+                
+                var buf = UnsafeMutablePointer<CChar>(readBuffer)
+                buf[length] = 0
+                // the C String must be null terminated
+                if let utf8String = String.fromCString(buf) {
+                    str = utf8String
+                }
+            }
+            readBuffer.dealloc(length)
+        }
+        return str
+        
+    }
+}
+
+public class Parser {
+    
+    private let stream: NSInputStream
+    private let format: Format
+    
+    @availability(iOS, introduced=4.0)
+    public convenience init?(fromUrl url: NSURL) {
+        self.init(fromStream: NSInputStream(URL: url)!)
+    }
+    
+    public init(fromStream stream: NSInputStream) {
+        self.stream = stream
+        self.format = Format.defaultFormat()
+    }
+    
+    public func parse() {
+        
+        let bufferSize = 1024
+        var buffer = Array<UInt8>(count: bufferSize, repeatedValue: 0)
+        
+        stream.open()
+        
+        if stream.hasBytesAvailable {
+            let bytesRead = stream.read(&buffer, maxLength: bufferSize)
+            if bytesRead >= 0 {
+                var output = NSString(bytes: &buffer, length: bytesRead, encoding: NSUTF8StringEncoding)
+                print(output)
+            } else {
+                print("[ERROR] No more byte to read")
+            }
+        }
+    }
+    
+}
---
+++ PurboSwiftCsv/PurboSwiftCsv.h
@@ -0,0 +1,19 @@
+//
+//  PurboSwiftCsv.h
+//  PurboSwiftCsv
+//
+//  Created by Purbo Mohamad on 11/29/14.
+//  Copyright (c) 2014 Purbo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PurboSwiftCsv.
+FOUNDATION_EXPORT double PurboSwiftCsvVersionNumber;
+
+//! Project version string for PurboSwiftCsv.
+FOUNDATION_EXPORT const unsigned char PurboSwiftCsvVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PurboSwiftCsv/PublicHeader.h>
+
+
